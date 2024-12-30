73497b372482099ddb71acc3b4c4ab0a60351cbe
Jean Victor <jeanvictor.fa@gmail.com>
2024-12-30 20:59:24 -0300

    Iniciando a navbar

--- frontend-nutri-smart/src/app/app.component.ts
+++ frontend-nutri-smart/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from "./components/navbar/navbar.component";
+import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, ReactiveFormsModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
