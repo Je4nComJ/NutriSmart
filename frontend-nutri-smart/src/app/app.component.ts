bab9bc38d9741042070ef8cd8446f879a80b3168
Jean Victor <jeanvictor.fa@gmail.com>
2025-01-05 16:20:17 -0300

    Navbar e Footer provisórios

--- frontend-nutri-smart/src/app/app.component.ts
+++ frontend-nutri-smart/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./components/navbar/navbar.component";
 import { ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, ReactiveFormsModule, CommonModule],
+  imports: [RouterOutlet, NavbarComponent, ReactiveFormsModule, CommonModule, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
